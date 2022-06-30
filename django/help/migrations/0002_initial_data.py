84a22e0f8969d988f8281856c51a3ca69c2e7677
Mike Allaway <36171746+mike-allaway@users.noreply.github.com>
2022-06-30 11:10:37 +0100

    help

---
+++ django/help/migrations/0002_initial_data.py
@@ -0,0 +1,35 @@
+from django.db import migrations
+from help import models
+
+
+def insert_help_items(apps, schema_editor):
+    """
+    Inserts objects into specified model
+    """
+
+    objects = [
+        {
+            "name": "Example video for helping demo the website to users",
+            "video": "https://www.youtube.com/watch?v=m80E1K75vDI",
+            "admin_published": True
+        },
+        {
+            "name": "Here's an example link for the help section",
+            "link": "https://www.birmingham.ac.uk/index.aspx",
+            "admin_published": True
+        },
+    ]
+
+    for object in objects:
+        models.HelpItem(**object).save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('help', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(insert_help_items),
+    ]
