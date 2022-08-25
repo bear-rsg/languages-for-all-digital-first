5f38adcf880ccb5eb20ae4f63679c459fa8fc0a0
Mike Allaway <36171746+mike-allaway@users.noreply.github.com>
2022-08-25 13:17:46 +0100

    user asked for minor tweaks

--- django/exercises/models.py
+++ django/exercises/models.py
@@ -538,13 +538,8 @@ class ExerciseFormatTranslation(models.Model):
     translation_source_text = models.TextField(blank=True, null=True, help_text='Optional if provided source image instead, otherwise required')
     translation_source_image = models.ImageField(upload_to='exercises-exerciseformat-translation', blank=True, null=True, help_text='Optional if provided source text instead, otherwise required')
     correct_translation = models.TextField()
-    correct_translation_audio = models.FileField(upload_to=AUDIO_UPLOAD_PATH, help_text=AUDIO_HELP_TEXT, blank=True, null=True)
     correct_answer_feedback = models.TextField(blank=True, null=True, help_text=CORRECT_ANSWER_FEEDBACK_HELP_TEXT)
     order = models.IntegerField(blank=True, null=True, help_text=EXERCISE_ITEM_ORDER_HELP_TEXT)
-
-    def has_audio(self):
-        return bool(self.correct_translation_audio)
-    has_audio.boolean = True  # sets tick/cross in admin dashboard
 
     def __str__(self):
         if self.exercise:
