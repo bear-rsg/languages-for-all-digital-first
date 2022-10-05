8092f886a058a3366784ef9417a6843b6b204c51
Mike Allaway <36171746+mike-allaway@users.noreply.github.com>
2022-10-05 17:56:06 +0100

    added comment to clarify class

--- django/account/storage.py
+++ django/account/storage.py
@@ -7,6 +7,10 @@ class ReplaceFileStorage(FileSystemStorage):
     """
     Replaces a file on a server (with the same name) instead of renaming it and retaining the old file.
 
+    If you have a FileField with file 'example.txt' uploaded and then upload another 'example.txt' to the same field,
+    by default this would rename the new file like 'example_938r38.txt' and retain both files.
+    Whereas this deletes the old 'example.txt' and uploads the new 'example.txt' as is. 
+
     Example of usage in a model field:
     my_file_field = models.FileField(upload_to='mediasubdir', storage=storage.ReplaceFileStorage(), blank=True, null=True)
     """
